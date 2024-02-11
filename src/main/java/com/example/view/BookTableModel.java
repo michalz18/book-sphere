cea2a1f5caa69c1b77a4d3aeaecd304ceaf2b7f2
Michał Zuzak <michalzuzak.2@gmail.com>
2024-02-11 14:49:59 +0100

    [ADD] BookTableModel to list all books in Bookstore

---
+++ src/main/java/com/example/view/BookTableModel.java
@@ -0,0 +1,56 @@
+package com.example.view;
+
+import com.example.model.Book;
+
+import javax.swing.table.AbstractTableModel;
+import java.util.List;
+
+public class BookTableModel extends AbstractTableModel {
+    private final String[] columnNames = {"ID", "Title", "Author", "Year", "Price", "Copies", "Edit", "Remove"};
+    private List<Book> books;
+
+    public BookTableModel(List<Book> books) {
+        this.books = books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+        fireTableDataChanged();
+    }
+
+    @Override
+    public int getRowCount() {
+        return books.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return columnNames.length;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        Book book = books.get(rowIndex);
+        return switch (columnIndex) {
+            case 0 -> book.getId().toString();
+            case 1 -> book.getTitle();
+            case 2 -> book.getAuthor();
+            case 3 -> book.getPublicationDate();
+            case 4 -> book.getPrice();
+            case 5 -> book.getNumberOfCopiesAvailable();
+            case 6 -> "Edit";
+            case 7 -> "Remove";
+            default -> null;
+        };
+    }
+
+    @Override
+    public String getColumnName(int column) {
+        return columnNames[column];
+    }
+
+    @Override
+    public boolean isCellEditable(int rowIndex, int columnIndex) {
+        return columnIndex >= 6;
+    }
+}
