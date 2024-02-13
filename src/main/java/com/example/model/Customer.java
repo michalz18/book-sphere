4815538e00402ff9b1b739b7ade91b27d5feef5a
Michał Zuzak <michalzuzak.2@gmail.com>
2024-02-13 13:07:44 +0100

    [ADD] Customer model

---
+++ src/main/java/com/example/model/Customer.java
@@ -0,0 +1,27 @@
+package com.example.model;
+
+import lombok.Data;
+
+import java.util.UUID;
+@Data
+public class Customer {
+    private UUID id;
+    private String firstName;
+    private String lastName;
+    private String city;
+    private String street;
+    private String postCode;
+    private String phoneNumber;
+    private String email;
+
+    public Customer(String firstName, String lastName, String city, String street, String postCode, String phoneNumber, String email) {
+        this.id = UUID.randomUUID();
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.city = city;
+        this.street = street;
+        this.postCode = postCode;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+}
